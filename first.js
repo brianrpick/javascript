72e3fa9f8a92616f748319e30b47a9a5c83fc222
Brian Pick <brianr.pick@gmail.com>
2016-12-07 21:18:22 -0800

    working phone cost and tax

--- first.js
+++ first.js
@@ -1,8 +1,9 @@
-bank = 100
-phone = 6
+var bank = 100
+var phone = 6
+const TAX_RATE = 0.09
 
-while bank <= 0 {
+while (bank >= phone) {
      console.log("Youre buying a phone!");
-     bank -- phone;
-     console.log( "Youre bank has" + bank + "left")
+     bank = bank - (phone + (  phone * TAX_RATE ));
+     console.log( "Youre bank has" + bank.toFixed( 2 ) + "left");
 }
