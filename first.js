1ff79b2655c63d62a56994973ce66c234c72eee8
Brian Pick <brianr.pick@gmail.com>
2016-12-11 10:48:46 -0800

    Added functions to calculate tax and syntax of values/output

--- first.js
+++ first.js
@@ -1,9 +1,22 @@
-var bank = 100
-var phone = 6
-const TAX_RATE = 0.09
+var phone = 5
+var acc = 3
+var bank = prompt( "Please enter your bank account balance")
+// const TAX_RATE = 0.09
+
+function tax(amt){
+        amt = amt * TAX_RATE + amt;
+        return amt; 
+      }
+function syntax(){
+        console.log( "You're bank has $" + bank.toFixed( 2 ) + " left")
+}
 
 while (bank >= phone) {
-     console.log("Youre buying a phone!");
-     bank = bank - (phone + (  phone * TAX_RATE ));
-     console.log( "Youre bank has" + bank.toFixed( 2 ) + "left");
+    console.log( "Youre buying a phone!" );
+    bank = bank - tax(phone);
+        if (bank >= acc ) {
+          bank = bank - tax(acc);
+          console.log( "You're adding on an accessory!");
+        }
+    syntax(bank);
 }
